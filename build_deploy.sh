f905f892e46379a8084c244ad1ca186df20bc96a
Daniel J Brieck Jr <djbrieck@gmail.com>
2025-06-20 08:24:34 -0700

    updated deployment script

--- build_deploy.sh
+++ build_deploy.sh
@@ -7,15 +7,15 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Attempting to add dist directory to ipfs"
-PUBLIC_HASH=$(ipfs add -rQ dist/ )
+echo "Attempting to add dist directory to ipfs cluster"
+PUBLIC_HASH=$(ipfs-cluster-ctl add -r -Q dist/ )
 if [ $? -ne 0 ]; then
     echo  "Add to IPFS failed!"
     exit 2
 fi
 
 echo "Received public hash of: $PUBLIC_HASH so lets try to update IPNS to it"
-ipfs name publish -key=unionStoplight $PUBLIC_HASH
+ipfs name publish -key=UnionStoplight $PUBLIC_HASH
 if [ $? -ne 0 ]; then
     echo  "Publish to IPNS failed."
     exit 3
